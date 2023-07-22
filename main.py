e88bae8fa761ddf762f2a350fd7baadf0d9af4ba
SergeiKasimov135 <101012933+SergeiKasimov135@users.noreply.github.com>
2023-07-23 01:31:51 +0400

    Modify

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def get_transactions(driver: webdriver.Chrome) -> int:
+def get_transactions(driver: webdriver.Chrome, url: str) -> int:
+    driver.get(url)
+    sleep(2.2)
     rows_of_transactions_table = driver.find_elements(By.CLASS_NAME, 'MuiTableRow-root')
-    return len(rows_of_transactions_table)
+    sleep(1.2)
+    return len(rows_of_transactions_table) - 1
 
 
-def main():
+def main() -> None:
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
 
@@ -17,16 +20,16 @@ def main():
     last_page = int(input("Введите номер последней страницы: "))
     user_address = input("Введите адрес кошелька: ")
     amount_of_transactions = 0
-    for number_of_page in range(1, last_page + 1):
-        driver.get(f"https://explorer.aptoslabs.com/account/{user_address}?network=mainnet&page={number_of_page}")
-        sleep(0.5)
-        amount = get_transactions(driver)
-        amount_of_transactions += amount
-        # print(f"[INFO] обработал страницу {number_of_page}")
 
-    print(amount_of_transactions)
+    if last_page > 1:
+        amount = get_transactions(driver, f"https://explorer.aptoslabs.com/account/{user_address}?network=mainnet&page=1") * last_page - 1
+        amount_on_last_page = get_transactions(driver, f"https://explorer.aptoslabs.com/account/{user_address}?network=mainnet&page={last_page}")
+        amount_of_transactions += amount + amount_on_last_page
+    else:
+        amount_of_transactions += get_transactions(driver, f"https://explorer.aptoslabs.com/account/{user_address}?network=mainnet&page={last_page}")
+
+    print(f"Количество транзакций: {amount_of_transactions}")
 
 
 if __name__ == "__main__":
     main()
-
